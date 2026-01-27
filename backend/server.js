2dbd0ebbf360cb7e3dd5d5783d08f6496ebd8964
Zainab <134186742+salazee@users.noreply.github.com>
2026-01-27 17:56:38 +0100

    remove socket

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const http = require("http");
-const app = require("./app");
+// const http = require("http");
+// const app = require("./app");
 
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const { initSocket } = require("./service/socket");
-initSocket(server);
+// // const { initSocket } = require("./service/socket");
+// // initSocket(server);
 
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server running on ${PORT}`);
+// });
--- backend/service/socket.js
+++ backend/service/socket.js
@@ -1,23 +1,23 @@
-let io;
+// let io;
 
-const initSocket = (server) => {
-  io = require("socket.io")(server, {
-    cors: {
-         origin: "*"
-         }
-  });
+// const initSocket = (server) => {
+//   io = require("socket.io")(server, {
+//     cors: {
+//          origin: "*"
+//          }
+//   });
 
-  io.on("connection", (socket) => {
-    socket.on("join", (userId) => {
-      socket.join(userId);
-    });
-  });
-};
+//   io.on("connection", (socket) => {
+//     socket.on("join", (userId) => {
+//       socket.join(userId);
+//     });
+//   });
+// };
 
-const sendNotification = (userId, payload) => {
-  if (io) {
-    io.to(userId.toString()).emit("notification", payload);
-  }
-};
+// const sendNotification = (userId, payload) => {
+//   if (io) {
+//     io.to(userId.toString()).emit("notification", payload);
+//   }
+// };
 
-module.exports = { initSocket, sendNotification };
+// module.exports = { initSocket, sendNotification };

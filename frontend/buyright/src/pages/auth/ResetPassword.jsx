5fca1dc96e9a8561b6116995e81f6ca452cc0dad
Zainab <134186742+salazee@users.noreply.github.com>
2026-01-31 15:44:42 +0100

    ../.. ->@

--- frontend/buyright/src/pages/auth/ResetPassword.jsx
+++ frontend/buyright/src/pages/auth/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useParams,useNavigate } from 'react-router-dom';
-import api from '../../lib/axios';
+import api from '@/lib/axios';
 import { toast } from 'react-hot-toast';
 
 export default function ResetPassword() {

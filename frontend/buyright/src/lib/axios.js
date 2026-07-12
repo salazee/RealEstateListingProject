963dc6656025791465475e3df45f448f7bd43e3d
Zainab <134186742+salazee@users.noreply.github.com>
2026-07-12 18:59:45 +0100

    updating Axios

--- frontend/buyright/src/lib/axios.js
+++ frontend/buyright/src/lib/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9000', // your backend
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000', // your backend
   withCredentials: false, // you're using JWT in Authorization header
 });
 
